8d157dbf29c90b19adbd76d0471639f60e70dc04
Ali Mohamed <alimohamed11907@gmail.com>
2025-05-17 06:23:08 +0300

    Update URl and in env

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,12 +1,11 @@
 import { sendEmail } from "../utils/sendMail.js";
 import { generateActivationEmail } from "../utils/generateHTML.js";
-import { API_URL } from "../constants.js";
 
 export const EmailService = {
   async sendActivationEmail({ email, activationCode }) {
     console.log(activationCode);
     // const link = `${FE_URL}/auth/confirmEmail/${activationCode}`; // Front
-    const link = `${API_URL}/api/auth/confirmEmail/${activationCode}`; // Backend
+    const link = `${process.env.API_URL}/api/auth/confirmEmail/${activationCode}`; // Backend
     const html = generateActivationEmail(link);
 
     return await sendEmail({
