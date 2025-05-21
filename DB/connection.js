8ab71a25f56d2a700b9423b48c509191448265f1
Ali Mohamed <alimohamed11907@gmail.com>
2025-05-21 10:22:12 +0300

    add use in Connection db

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectionDB = async () => {
   return await mongoose
-    .connect(process.env.CONNECTIONURL)
+    .connect(process.env.CONNECTIONURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10 ثواني مهلة للاتصال
+    })
     .then(() => console.log("DB Connected....!"))
     .catch((error) => console.log("Error", error));
 };
