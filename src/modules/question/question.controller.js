6eea28d3b8acb31804fcf108d476d641ec8eaa0b
Ali Mohamed <alimohamed11907@gmail.com>
2025-05-25 20:52:12 +0300

    add and get all question for an exam

--- src/modules/question/question.controller.js
+++ src/modules/question/question.controller.js
@@ -1,14 +1,27 @@
+import { Exam } from "../../../DB/models/exam.model.js";
 import { Question } from "../../../DB/models/question.model.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import sendResponse from "../../utils/response.js";
 
+// Create Question - Admin only
 export const createQuestion = asyncHandler(async (req, res, next) => {
   const { examId } = req.params;
+  const {
+    questionText,
+    questionType,
+    options,
+    correctAnswer,
+    modelAnswer,
+    correctText,
+    points,
+    difficulty,
+  } = req.body;
   // Check if exam exists
   const exam = await Exam.findById(examId);
   if (!exam) {
     throw new ApiError(404, "Exam not found");
   }
+
   // Check if user is the creator of the exam or admin
   if (
     exam.createdBy.toString() !== req.user._id.toString() &&
@@ -17,7 +30,14 @@ export const createQuestion = asyncHandler(async (req, res, next) => {
     throw new ApiError(403, "Not authorized to add questions to this exam");
   }
   const question = await Question.create({
-    ...req.body,
+    questionText,
+    questionType,
+    options: questionType === "multiple-choice" ? options : undefined,
+    correctAnswer: questionType === "true-false" ? correctAnswer : undefined,
+    modelAnswer: questionType === "essay" ? modelAnswer : undefined,
+    correctText: questionType === "fill-blank" ? correctText : undefined,
+    points: points || 1,
+    difficulty: difficulty || "medium",
     exam: examId,
   });
   if (!question) return next(new ApiError(400, "Can not create question"));
@@ -26,5 +46,52 @@ export const createQuestion = asyncHandler(async (req, res, next) => {
     statusCode: 201,
     data: question,
     message: "Question created successfully",
+  });
+});
+
+// Get All Questions for an Exam
+export const getQuestionsByExam = asyncHandler(async (req, res, next) => {
+  const { examId } = req.params;
+  const {
+    page = 1,
+    limit = 10,
+    questionType,
+    questionText,
+    difficulty,
+    points,
+  } = req.query;
+
+  // Check if exam exists
+  const exam = await Exam.findById(examId);
+  if (!exam) return next(new ApiError(404, "Exam not found"));
+
+  const filter = { exam: examId };
+  if (difficulty) filter.difficulty = difficulty;
+  if (questionType) filter.questionType = questionType;
+
+  if (questionText) filter.questionText = { $regex: questionText, option: "i" };
+  if (points) filter.points = {};
+
+  const questions = await Question.find(filter)
+    .populate("exam", "subject level duration")
+    .sort({ createdAt: -1 })
+    .limit(limit * 1)
+    .skip((page - 1) * limit);
+
+  const total = await Question.countDocuments(filter);
+
+  sendResponse(res, {
+    data: {
+      total,
+      totalPages: Math.ceil(total / limit),
+      currentPage: Number(page),
+      questions,
+      exam: {
+        _id: exam._id,
+        subject: exam.subject,
+        level: exam.level,
+      },
+    },
+    message: "Questions retrieved successfully",
   });
 });
--- src/modules/question/question.router.js
+++ src/modules/question/question.router.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { authorizeRole, protect } from "../../middleware/auth.middleware.js";
 import { createQuestionSchema } from "./question.validation.js";
-import { createQuestion } from "./question.controller.js";
+import { createQuestion, getQuestionsByExam } from "./question.controller.js";
 import { isValid } from "../../middleware/validation.middleware.js";
+import { idSchema } from "../exam/exam.validation.js";
 
 const router = Router({ mergeParams: true });
 
 // Create Question
 router
   .route("/")
-  .all(protect, authorizeRole("admin"))
-  .post(isValid(createQuestionSchema), createQuestion);
+  .all(protect)
+  .post(authorizeRole("admin"), isValid(createQuestionSchema), createQuestion)
+  .get(isValid(idSchema), getQuestionsByExam);
 
 export default router;
